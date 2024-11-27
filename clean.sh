e683c3988ba10df5e42ba01f1a5beb931a1b077e
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-11-27 17:52:56 +0100

    better clean.sh

--- clean.sh
+++ clean.sh
@@ -1,2 +1,6 @@
+rm -rf target/checkout
+rm -rf target/prepare
+rm -rf pom.xml.releaseBackup
+git checkout pom.xml
 git tag -d password-validator-1.0.0
 git push origin --delete refs/tags/password-validator-1.0.0
