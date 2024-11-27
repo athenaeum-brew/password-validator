ca9d747a9dd7238a097e4858b79084496a7b8bd3
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-11-27 17:53:30 +0100

    -X

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 echo mvn release:prepare -DreleaseVersion=1.0.0 -DdevelopmentVersion=2.0.0-SNAPSHOT -Dtag1.0.0 -DautoVersionSubmodules=true
-mvn release:prepare -DreleaseVersion=1.0.0 -DdevelopmentVersion=2.0.0-SNAPSHOT -Dtag1.0.0 -DautoVersionSubmodules=true
+mvn release:prepare -DreleaseVersion=1.0.0 -DdevelopmentVersion=2.0.0-SNAPSHOT -Dtag1.0.0 -DautoVersionSubmodules=true -X
